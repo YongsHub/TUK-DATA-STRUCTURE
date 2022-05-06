7508cc8cb802e448a116e071e6727a327a3f2c98
wooyoung <102501814+lwoolyoung@users.noreply.github.com>
2022-05-06 23:26:42 +0900

    Create 중간보고1.py
    양우영 회전하는 큐

---
+++ 양우영_숙제/중간보고1.py
@@ -0,0 +1,10 @@
+import sys
+from collections import deque
+N, M= map(int,input().split())
+number = list(map(int,input(),split()))
+data = deque([i for i in range(1,n+1)])
+
+count = 0
+for num in number:
+    while:
+        if data[0] == num:
